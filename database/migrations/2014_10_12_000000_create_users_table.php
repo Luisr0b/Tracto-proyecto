f9e4a280a61aebcd1ad3fd808ce923152047fc45
Luis Uriel Robles Estrada <89150007+Luisr0b@users.noreply.github.com>
2023-05-15 23:56:29 -0600

    Cambios 1.2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firtsname');
+            $table->string('lastname')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('start_date')->requerid();
+            $table->timestamp('end_date')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+
+            $table->unsignedBigInteger('created_by')->index();
+            $table->unsignedBigInteger('updated_by')->index();
         });
     }
 
---
+++ database/migrations/2023_05_16_034945_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('type_id')->nullable();
+            $table->unsignedSmallInteger('priority_id')->required();
+            $table->unsignedBigInteger('requester_user_id')->required();
+            $table->string('name')->required();
+            // Aquí va el id del comentario que sirve de descripción
+            $table->boolean('flag_status')->default(true);
+            $table->timestamps();
+            $table->unsignedBigInteger('created_by')->index();
+            $table->unsignedBigInteger('updated_by')->index();
+
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
+            $table->foreign('priority_id')->references('id')->on('priorities');
+            $table->foreign('requester_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
