5a14d47b53c03f764f1bd9fdbb16ebd29cb2ddb5
Luis Uriel Robles Estrada <89150007+Luisr0b@users.noreply.github.com>
2023-05-16 13:38:28 -0600

    Cambios 1.3

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('layouts.app');
 });
 
 Route::middleware([
@@ -27,7 +27,7 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/', function () {
-        return view('index');
+        return view('layouts.app');
     })->name('index');
 });
 
