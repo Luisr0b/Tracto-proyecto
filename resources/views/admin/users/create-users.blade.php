ac6a9954e32b3bbc53df539cb9ce6317434a9e69
Luis Uriel Robles Estrada <89150007+Luisr0b@users.noreply.github.com>
2023-05-17 13:12:27 -0600

    Cambios 1.6

--- resources/views/admin/users/create-users.blade.php
+++ resources/views/admin/users/create-users.blade.php
@@ -9,16 +9,57 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-
             <h3 class="card-title">Creacion de Usuarios</h3>
-
-
         </div>
 
         <div class="card-body">
+            <div class="row">
+                <form action="/users" method="POST">
+                        @csrf
+                    <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                        <label for="first_name">Nombres</label><br>
+                        <input type="text" name="first_name" id="first_name" value="{{ old('first_name') }}" required><br>
+                        @error('first_name')
+                            <h5>{{$meesage }}</h5>
+                        @enderror
+                    </div>
 
+                    <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                        <label for="last_name">Apellidos</label><br>
+                        <input type="text" name="last_name" id="last_name" value="{{ old('last_name') }}" required><br>
+                        @error('last_name')
+                            <h5>{{$meesage }}</h5>
+                        @enderror
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                        <label for="email">Correo</label><br>
+                        <input type="text" name="email" id="email" value="{{ old('email') }}" required><br>
+                        @error('email')
+                            <h5>{{$meesage }}</h5>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <label for="start-date">Fecha de ingreso</label><br>
+                        <input type="date" name="start-date" id="start-date" min="1950-01-01" max="2099-01-01">
+                        @error('start-date')
+                            <h5>{{$meesage }}</h5>
+                        @enderror
+                    </div>
+                </form>
+            </div>
         </div>
 
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="float-right">
+                <button type="submit" class="btn btn-success">Agregar empleado</button>
+                <a href="/users" class="btn btn-outline-danger">Regresar</a>
+            </div>
+        </div>
     </div>
 @stop
 
